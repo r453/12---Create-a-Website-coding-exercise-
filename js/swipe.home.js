876dca528553a51a86ef8e2a4f9c3a2b1207c7bf
afmansne <r453.2@kusti.net>
2023-04-10 18:52:21 +0300

    Add files via upload
    New features

---
+++ js/swipe.home.js
@@ -0,0 +1,25 @@
+// Sivun vaihto pyyhkäisemällä //
+// https://stackoverflow.com/questions/45648886/swipe-left-right-for-a-webpage
+
+var start = null;
+window.addEventListener("touchstart",function(event){
+  if(event.touches.length === 1){
+     start = event.touches.item(0).clientX;
+   }else{
+     start = null;
+   }
+ });
+
+window.addEventListener("touchend",function(event){
+  // tähän ehdotettiin stackoverflow:ssa relatiivista arvoa mutta en saanut siihen oikein järkevää toiminnallisuutta eri näytöillä //
+  var offset = 100;
+    if(start){
+      var end = event.changedTouches.item(0).clientX;
+      if(end > start + offset){
+      // ei tehdä mitään
+      }
+      if(end < start - offset ){
+        location.replace("./about.html")
+      }
+   }
+ });
